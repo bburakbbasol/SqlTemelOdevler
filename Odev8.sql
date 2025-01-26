52da17e2a960c017a410f7713a381637a4e9a3cb
Burak Başol <117232704+bburakbbasol@users.noreply.github.com>
2025-01-26 12:51:57 +0300

    Add files via upload
    ODEV8

---
+++ Odev8.sql
@@ -0,0 +1,73 @@
+
+--TABLO oluşturma
+CREATE TABLE employee(
+
+id INTEGER PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+birthday DATE NOT NULL,
+ email VARCHAR(100)
+);
+
+
+
+--- Mockaroo dan veri ekleme 
+insert into employee (id, name, email, birthday) values (1, 'Ray', 'rdictus0@nationalgeographic.com', '2016-02-28');
+insert into employee (id, name, email, birthday) values (2, 'Efrem', 'ebaines1@ning.com', '1978-06-06');
+insert into employee (id, name, email, birthday) values (3, 'Bernardine', 'bgumly2@networksolutions.com', '1988-01-05');
+insert into employee (id, name, email, birthday) values (4, 'Rhodia', 'rwilfinger3@nytimes.com', '1969-03-27');
+insert into employee (id, name, email, birthday) values (5, 'Mile', 'mgethyn4@pcworld.com', '2015-09-03');
+insert into employee (id, name, email, birthday) values (6, 'Milka', 'mgreasley5@nifty.com', '1973-11-24');
+insert into employee (id, name, email, birthday) values (7, 'Olva', 'okillen6@epa.gov', '1952-05-05');
+insert into employee (id, name, email, birthday) values (8, 'Dalt', 'dmitchiner7@printfriendly.com', '1994-10-20');
+insert into employee (id, name, email, birthday) values (9, 'Ellery', 'emoorman8@google.it', '2018-10-22');
+insert into employee (id, name, email, birthday) values (10, 'Annamarie', 'ahalfhead9@admin.ch', '1967-12-28');
+insert into employee (id, name, email, birthday) values (11, 'Henka', 'hvedeneeva@creativecommons.org', '1949-01-15');
+insert into employee (id, name, email, birthday) values (12, 'Addie', 'awhelbandb@facebook.com', '1955-01-24');
+insert into employee (id, name, email, birthday) values (13, 'Buddy', 'bmarinic@marketwatch.com', '2024-06-15');
+insert into employee (id, name, email, birthday) values (14, 'Bendite', 'bbrigshawd@nasa.gov', '2020-02-21');
+insert into employee (id, name, email, birthday) values (15, 'Sayer', 'sbolsteridgee@hexun.com', '1950-07-28');
+insert into employee (id, name, email, birthday) values (16, 'Damaris', 'dentwhistlef@msn.com', '2013-07-12');
+insert into employee (id, name, email, birthday) values (17, 'Mollie', 'mtoffulg@goo.gl', '1969-07-29');
+insert into employee (id, name, email, birthday) values (18, 'Amii', 'areuvenh@sina.com.cn', '1955-01-22');
+insert into employee (id, name, email, birthday) values (19, 'Kare', 'kwithnalli@psu.edu', '1946-03-12');
+insert into employee (id, name, email, birthday) values (20, 'Zollie', 'zstinchcombej@zdnet.com', '1973-02-26');
+insert into employee (id, name, email, birthday) values (21, 'Jeremiah', 'jcicceralek@columbia.edu', '1980-09-25');
+insert into employee (id, name, email, birthday) values (22, 'Ethelred', 'eoaklandl@goodreads.com', '1977-03-03');
+insert into employee (id, name, email, birthday) values (23, 'Cynde', 'cgeorgm@miibeian.gov.cn', '2022-06-13');
+insert into employee (id, name, email, birthday) values (24, 'Sapphire', 'sruslinn@house.gov', '1986-05-06');
+insert into employee (id, name, email, birthday) values (25, 'Cedric', 'cketleyo@hostgator.com', '1974-10-17');
+insert into employee (id, name, email, birthday) values (26, 'Tanhya', 'tblackaderp@ycombinator.com', '2009-10-15');
+insert into employee (id, name, email, birthday) values (27, 'Mattias', 'mkarpmanq@discuz.net', '2018-03-27');
+insert into employee (id, name, email, birthday) values (28, 'Lira', 'lgartinr@aol.com', '1990-12-17');
+insert into employee (id, name, email, birthday) values (29, 'Parsifal', 'pwahners@upenn.edu', '1948-11-15');
+insert into employee (id, name, email, birthday) values (30, 'Darill', 'dmunehayt@bing.com', '1957-05-16');
+insert into employee (id, name, email, birthday) values (31, 'Regan', 'rschenkelu@ezinearticles.com', '2007-09-24');
+insert into employee (id, name, email, birthday) values (32, 'Mallory', 'mjuszczykv@google.com.br', '1999-07-07');
+insert into employee (id, name, email, birthday) values (33, 'Selig', 'smarienw@linkedin.com', '1966-06-10');
+insert into employee (id, name, email, birthday) values (34, 'Rhiamon', 'rruncimanx@mit.edu', '2018-12-18');
+insert into employee (id, name, email, birthday) values (35, 'Dugald', 'dmoukesy@cnet.com', '1970-12-20');
+insert into employee (id, name, email, birthday) values (36, 'Ambrosi', 'aweetchz@fda.gov', '2019-05-07');
+insert into employee (id, name, email, birthday) values (37, 'Laurice', 'lbloyes10@acquirethisname.com', '1990-04-30');
+insert into employee (id, name, email, birthday) values (38, 'Mil', 'mtossell11@smugmug.com', '1991-04-25');
+insert into employee (id, name, email, birthday) values (39, 'Karel', 'kjobson12@theguardian.com', '2008-04-01');
+insert into employee (id, name, email, birthday) values (40, 'Rurik', 'rlaydon13@blogspot.com', '1951-10-25');
+insert into employee (id, name, email, birthday) values (41, 'Garnette', 'gblackman14@engadget.com', '1978-12-09');
+insert into employee (id, name, email, birthday) values (42, 'Chickie', 'caked15@php.net', '2011-10-12');
+insert into employee (id, name, email, birthday) values (43, 'Janka', 'jbraban16@alexa.com', '1981-04-15');
+insert into employee (id, name, email, birthday) values (44, 'Chance', 'cbucktharp17@wired.com', '1976-12-17');
+insert into employee (id, name, email, birthday) values (45, 'Harri', 'hpaule18@fastcompany.com', '1960-06-23');
+insert into employee (id, name, email, birthday) values (46, 'Jaclin', 'jdober19@sitemeter.com', '2006-02-01');
+insert into employee (id, name, email, birthday) values (47, 'Ted', 'tbassindale1a@vk.com', '2021-05-17');
+insert into employee (id, name, email, birthday) values (48, 'Vernen', 'vsiberry1b@zdnet.com', '1973-04-11');
+insert into employee (id, name, email, birthday) values (49, 'Raychel', 'rjeandon1c@taobao.com', '1951-07-24');
+insert into employee (id, name, email, birthday) values (50, 'Gabby', 'gdullard1d@ebay.co.uk', '1966-03-03');
+
+
+--Veri güncelleme 
+
+update employee
+set birthday='1999-11-12'
+where name like 'A%';
+-- veri silme
+DELETE FROM employee
+WHERE birthday<'1970-11-11'
